967e8556fccab2850fe616c8e5cb9949ee93be06
timlyo <timlyomaddison@gmail.com>
2016-03-27 13:23:05 +0100

    Formatting changes

--- website/routes/routes.py
+++ website/routes/routes.py
@@ -10,36 +10,36 @@ from flask.ext.uploads import IMAGES, secure_filename
 
 
 def is_image_file(filename: str) -> bool:
-    return "." in filename and filename.rsplit(".", 1)[1] in IMAGES
+	return "." in filename and filename.rsplit(".", 1)[1] in IMAGES
 
 
 @app.route("/")
 @app.route("/index")
 def index():
-    news = reversed(data.get_latest_articles(5))
-    return render_template("index.html", news=news, group=None)
+	news = reversed(data.get_latest_articles(5))
+	return render_template("index.html", news=news, group=None)
 
 
 @app.route("/upload", methods=["GET", "POST"])
 @login_required
 def upload_image():
-    print("upload")
-    if request.method == "POST":
-        file = request.files["file"]
-        if file:
-            if not is_image_file(file.filename):
-                print("Error", file, " not image")
-                return "Not an image", 418
+	print("upload")
+	if request.method == "POST":
+		file = request.files["file"]
+		if file:
+			if not is_image_file(file.filename):
+				print("Error", file, " not image")
+				return "Not an image", 418
 
-            data.save_image(file)
-            return "ok"
+			data.save_image(file)
+			return "ok"
 
-    return render_template("upload.html")
+	return render_template("upload.html")
 
 
 @app.route("/images")
 def images():
-    return render_template("images.html")
+	return render_template("images.html")
 
 
 # @app.route("/<group>/badges")
@@ -50,37 +50,37 @@ def images():
 
 @app.route("/<group>")
 def serve_group(group):
-    tab = "about"
-    news = data.get_latest_articles(5, unit=group)
-    result = None
-    try:
-        result = render_template("group/{}.html".format(group), group=group, news=news, tab=tab)
-    except jinja2.exceptions.TemplateNotFound as e:
-        print("No template for group:", group)
-        return "not found", 404
-    return result
+	tab = "about"
+	news = data.get_latest_articles(5, unit=group)
+	result = None
+	try:
+		result = render_template("group/{}.html".format(group), group=group, news=news, tab=tab)
+	except jinja2.exceptions.TemplateNotFound as e:
+		print("No template for group:", group)
+		return "not found", 404
+	return result
 
 
 @app.route("/<group>/program")
 def show_program(group):
-    tab = "program"
-    if group == "cubs":
-        thor_calendar = data.get_program("Thor")
-        woden_calendar = data.get_program("Woden")
+	tab = "program"
+	if group == "cubs":
+		thor_calendar = data.get_program("Thor")
+		woden_calendar = data.get_program("Woden")
 
-        return render_template("program.html", group="cubs", programs=[thor_calendar, woden_calendar], tab=tab)
-    if group == "scouts":
-        program = data.get_program("Scouts")
+		return render_template("program.html", group="cubs", programs=[thor_calendar, woden_calendar], tab=tab)
+	if group == "scouts":
+		program = data.get_program("Scouts")
 
-        return render_template("program.html", group="scouts", programs=[program])
-    else:
-        return "Program not found"
+		return render_template("program.html", group="scouts", programs=[program])
+	else:
+		return "Program not found"
 
 
 @app.route("/<group>/contact")
 def contact(group):
-    tab = "contact"
-    return render_template("contact.html", group=group, tab=tab)
+	tab = "contact"
+	return render_template("contact.html", group=group, tab=tab)
 
 
 @app.route("/admin")
@@ -88,26 +88,28 @@ def contact(group):
 def admin():
 	return redirect("/admin/articles")
 
+
 @app.route("/admin/articles")
 @login_required
 def admin_articles():
-    program_list = data.get_program_list()
-    articles = reversed(data.get_latest_articles(all=True))
-    news_count = data.get_article_count()
+	program_list = data.get_program_list()
+	articles = reversed(data.get_latest_articles(all=True))
+	news_count = data.get_article_count()
 
-    return render_template("admin/admin.html", program_list=program_list, articles=articles, news_count=news_count)
+	return render_template("admin/admin.html", program_list=program_list, articles=articles, news_count=news_count)
+
 
 @app.route("/admin/programs")
 @login_required
 def admin_programs():
-    program_list = data.get_program_list()
-    articles = reversed(data.get_latest_articles(all=True))
-    news_count = data.get_article_count()
+	program_list = data.get_program_list()
+	articles = reversed(data.get_latest_articles(all=True))
+	news_count = data.get_article_count()
 
-    return render_template("admin/programs.html", program_list=program_list, articles=articles, news_count=news_count)
+	return render_template("admin/programs.html", program_list=program_list, articles=articles, news_count=news_count)
 
 
 @app.route("/editProgram/<name>", methods=["GET"])
 @login_required
 def edit_program(name):
-    return render_template("admin/editProgram.html", name=name)
+	return render_template("admin/editProgram.html", name=name)
