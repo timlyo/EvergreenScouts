8182ef08a20412fc64c8ebbb96c9c857b0400622
timlyo <timlyomaddison@gmail.com>
2016-03-27 16:30:25 +0100

    Tied rethinkdb in for users

--- website/data.py
+++ website/data.py
@@ -23,11 +23,6 @@ articles = database.table("articles")
 programs = database.table("programs")
 users = database.table("users")
 
-program_db = TinyDB(app.config["DATA_DIRECTORY"] + "/programs.json")
-users_db = TinyDB(app.config["DATA_DIRECTORY"] + "/users.json")
-news_db = TinyDB(app.config["DATA_DIRECTORY"] + "/news.json")
-images_db = TinyDB(app.config["DATA_DIRECTORY"] + "/images.json")
-
 pool = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
 
 
@@ -37,48 +32,48 @@ pool = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
 
 
 def get_program_list():
-    program_list = program_db.all()
-    return program_list
+	program_list = program_db.all()
+	return program_list
 
 
 def get_program(name: str):
-    print("Getting program named:", name)
-    connect_to_db()
+	print("Getting program named:", name)
+	connect_to_db()
 
-    result = programs.get_all(name, index="name").run()
-    result = list(result)
-    print(result)
-    if len(result) == 0:
-        # TODO none found case
-        pass
-    elif len(result) > 1:
-        return result[0]
-    else:
-        return result[0]
+	result = programs.get_all(name, index="name").run()
+	result = list(result)
+	print(result)
+	if len(result) == 0:
+		# TODO none found case
+		pass
+	elif len(result) > 1:
+		return result[0]
+	else:
+		return result[0]
 
 
 def get_programs() -> list:
-    """Return list of all programs"""
-    result = program_db.all()
-    print("programs", result)
-    return result
+	"""Return list of all programs"""
+	result = program_db.all()
+	print("programs", result)
+	return result
 
 
 def save_program(data: dict, name: str):
-    """takes the data from a dictionary and saves to disk"""
-    print("Saving", name, "program")
-    print(data)
-    program_db.update({"events": data}, where("name") == name)
+	"""takes the data from a dictionary and saves to disk"""
+	print("Saving", name, "program")
+	print(data)
+	program_db.update({"events": data}, where("name") == name)
 
 
 def get_json(file: str) -> dict:
-    data = json.load(open(file))
-    return data
+	data = json.load(open(file))
+	return data
 
 
 def get_remote_json(url: str) -> dict:
-    response = pool.request("GET", url)
-    return json.loads(response.data.decode("utf-8"))
+	response = pool.request("GET", url)
+	return json.loads(response.data.decode("utf-8"))
 
 
 ##################
@@ -86,80 +81,82 @@ def get_remote_json(url: str) -> dict:
 ##################
 
 def get_user(id: str):
-    result = users_db.search(where("id") == id)
+	connect_to_db()
+	result = list(users.get_all(id, index="id").run())
+	# result = users_db.search(where("id") == id)
 
-    if len(result) == 1:
-        return result[0]
-    elif len(result) == 0:
-        raise KeyError("No user with id " + str(id))
-    else:
-        print("Error: duplicate ids", result, file=sys.stderr)
-        return None
+	if len(result) == 1:
+		return result[0]
+	elif len(result) == 0:
+		raise KeyError("No user with id " + str(id))
+	else:
+		print("Error: duplicate ids", result, file=sys.stderr)
+		return None
 
 
 ##################
 # Article stuff
 ##################
 
-def get_latest_articles(start: int=0, end: int=None, unit="", all=False):
-    connect_to_db()
+def get_latest_articles(start: int = 0, end: int = None, unit="", all=False):
+	connect_to_db()
 
-    result = None
-    if all:
-        result = articles.order_by(index="created").run()
-    elif unit == "":
-        result = articles.get_all("published", index="state").run()
-    else:
-        result = articles.get_all(unit, index="unit").run()  # TODO and operation
+	result = None
+	if all:
+		result = articles.order_by(index="created").run()
+	elif unit == "":
+		result = articles.get_all("published", index="state").run()
+	else:
+		result = articles.get_all(unit, index="unit").run()  # TODO and operation
 
-    result = list(result)
+	result = list(result)
 
-    if end:
-        return result[start:end]
-    else:
-        return result
+	if end:
+		return result[start:end]
+	else:
+		return result
 
 
 def get_article_count():
-    return len(news_db)
+	return len(news_db)
 
 
 def get_article_by_title(title: str):
-    connect_to_db()
-    result = articles.get_all(title, index="title").run()
+	connect_to_db()
+	result = articles.get_all(title, index="title").run()
 
-    try:
-        article = result.next()
-        return article
-    except rethinkdb.net.DefaultCursorEmpty:
-        return None
+	try:
+		article = result.next()
+		return article
+	except rethinkdb.net.DefaultCursorEmpty:
+		return None
 
 
 def get_article(article_id):
-    connect_to_db()
-    result = articles.get(article_id).run()
-    return result
+	connect_to_db()
+	result = articles.get(article_id).run()
+	return result
 
 
 def update_article(article_id, data, body=None, title=None, outline=None, unit=None, state=None):
-    print("Update to article", article_id)
-    print(data)
-    date = rethinkdb.now()
-    connect_to_db()
-    articles.get(article_id).update(data).run()
-    articles.get(article_id).update({"updated": date}).run()
+	print("Update to article", article_id)
+	print(data)
+	date = rethinkdb.now()
+	connect_to_db()
+	articles.get(article_id).update(data).run()
+	articles.get(article_id).update({"updated": date}).run()
 
 
 def create_new_article():
-    connect_to_db()
+	connect_to_db()
 
-    date = rethinkdb.now()
-    articles.insert({
-        "created": date,
-        "updated": None,
-        "state": "editing",
-        "title": "New Article",
-    }).run()
+	date = rethinkdb.now()
+	articles.insert({
+		"created": date,
+		"updated": None,
+		"state": "editing",
+		"title": "New Article",
+	}).run()
 
 
 ####################
@@ -167,78 +164,78 @@ def create_new_article():
 ####################
 
 def save_image(file: FileStorage):
-    thumbnail_size = 240, 240
-    directory = app.config["IMAGE_DIRECTORY"]
+	thumbnail_size = 240, 240
+	directory = app.config["IMAGE_DIRECTORY"]
 
-    # Compute hash
-    file_hash = hashlib.md5(file.read()).hexdigest()
-    file.seek(0)
+	# Compute hash
+	file_hash = hashlib.md5(file.read()).hexdigest()
+	file.seek(0)
 
-    file_name = file_hash + ".jpg"
-    file_path = os.path.join(directory, file_name)
-    thumbnail_name = file_hash + "_thumb.jpg"
-    thumbnail_path = os.path.join(directory, thumbnail_name)
+	file_name = file_hash + ".jpg"
+	file_path = os.path.join(directory, file_name)
+	thumbnail_name = file_hash + "_thumb.jpg"
+	thumbnail_path = os.path.join(directory, thumbnail_name)
 
-    image = Image.open(file)
-    size = image.size
+	image = Image.open(file)
+	size = image.size
 
-    if not os.path.isfile(file_path):
-        # save as jpg for size efficiency
-        image.save(file_path)
-        print("Saved image as", file_path)
+	if not os.path.isfile(file_path):
+		# save as jpg for size efficiency
+		image.save(file_path)
+		print("Saved image as", file_path)
 
-    if not os.path.isfile(thumbnail_path):
-        # create thumbnail
-        image = Image.open(os.path.join(directory, file_name))
-        image.thumbnail(thumbnail_size, Image.ANTIALIAS)
-        image.save(os.path.join(directory, thumbnail_name))
-        print("saved thumbnail as", thumbnail_path)
+	if not os.path.isfile(thumbnail_path):
+		# create thumbnail
+		image = Image.open(os.path.join(directory, file_name))
+		image.thumbnail(thumbnail_size, Image.ANTIALIAS)
+		image.save(os.path.join(directory, thumbnail_name))
+		print("saved thumbnail as", thumbnail_path)
 
-    store_image(file_hash, size)
+	store_image(file_hash, size)
 
 
 def store_image(file_hash: str, size: tuple):
-    """ Insert image into the database
-    :param file_hash: hash of the file
-    :param size: tuple of image size (x, y)
-    """
+	""" Insert image into the database
+	:param file_hash: hash of the file
+	:param size: tuple of image size (x, y)
+	"""
 
-    date = datetime.datetime.now().isoformat()
+	date = datetime.datetime.now().isoformat()
 
-    images_db.insert({
-        "file": file_hash,
-        "date": date,
-        "size": size,
-        "name": ""
-    })
+	images_db.insert({
+		"file": file_hash,
+		"date": date,
+		"size": size,
+		"name": ""
+	})
 
 
 def get_images(id_list=None, file_list=None, date=None, limit=None, location=None, get_all=False) -> list:
-    if not limit:
-        limit = 50
+	if not limit:
+		limit = 50
 
-    if get_all:
-        return images_db.all()[:limit]
+	if get_all:
+		return images_db.all()[:limit]
 
-    images = []
-    if id_list:
-        for id in id_list.split(","):
-            try:
-                id = int(id)
-            except ValueError:
-                continue
-            result = images_db.get(eid=id)
-            if result:
-                images.append(result)
+	images = []
+	if id_list:
+		for id in id_list.split(","):
+			try:
+				id = int(id)
+			except ValueError:
+				continue
+			result = images_db.get(eid=id)
+			if result:
+				images.append(result)
 
-    if file_list:
-        for file in file_list.split(","):
-            result = images_db.search(where("file") == file)
-            if result:
-                images.append(result)
+	if file_list:
+		for file in file_list.split(","):
+			result = images_db.search(where("file") == file)
+			if result:
+				images.append(result)
 
-    return images[:limit]
+	return images[:limit]
 
 
 def connect_to_db():
-    rethinkdb.connect("localhost", 28015).repl()
+	rethinkdb.connect("localhost", 28015).repl()
